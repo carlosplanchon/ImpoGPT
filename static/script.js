088b064c870dc30012af925205cd99b7302a75bf
Carlos Andrés Planchón Prestes <carlosandresplanchonprestes@gmail.com>
2023-03-17 21:13:44 -0300

    Adding message when API KEY is not in the text field.

--- static/script.js
+++ static/script.js
@@ -8,19 +8,25 @@ const sourcesArea = document.getElementById("sources");
 sendPromptButton.addEventListener("click", async () => {
     responseArea.innerHTML = "Procesando... puede tardar en el entorno de un minuto.";
     sourcesArea.innerHTML = "";
+
     const apiKey = apiKeyInput.value;
     const prompt = promptInput.value;
-    const response = await fetch("/send_question", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            api_key: apiKey,
-            prompt,
-            law_filter: null
-        })
-    });
+
+    if (apiKey === "") {
+        responseArea.innerHTML = "INGRESE UNA APIKEY DE OPENAI. Tutorial: https://blog.nubecolectiva.com/como-obtener-una-api-key-de-openai-chatgpt/"
+    } else {
+      const response = await fetch("/send_question", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+              api_key: apiKey,
+              prompt,
+              law_filter: null
+          })
+      });
+    }
 
     const data = await response.json();
     console.log("--- RESPONSE ---");
